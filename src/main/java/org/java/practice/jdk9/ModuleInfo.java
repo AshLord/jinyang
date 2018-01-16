be419450924894f5e8bb962158e54357ab68c342
yang.jin <yang.jin@msxf.com>
2018-01-16 23:45:12 +0800

    jdk9

---
+++ src/main/java/org/java/practice/jdk9/ModuleInfo.java
@@ -0,0 +1,31 @@
+package org.java.practice.jdk9;
+
+public class ModuleInfo {
+
+    /**
+     * module org.java.practice {
+
+     requires java.base;
+     requires java.compiler;
+     requires java.sql;
+     requires java.xml;
+
+     requires tomcat.embed.core;
+
+     requires spring.aop;
+     requires spring.web;
+     requires spring.beans;
+     requires spring.core;
+     requires spring.context;
+
+     requires commons.io;
+
+     requires junit;
+     requires jedis;
+     requires xstream;
+     requires fastjson;
+     requires httpcore;
+     requires httpclient;
+     }
+     */
+}
